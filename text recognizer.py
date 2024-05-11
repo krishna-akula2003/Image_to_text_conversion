fa3762628e986b412a508fb8f5143e0f44a07e1d
Chinni Krishna Akula <138143431+krishna-akula2003@users.noreply.github.com>
2024-05-11 18:34:26 +0530

    Update text recognizer.py

--- text recognizer.py
+++ text recognizer.py
@@ -1,20 +1,13 @@
 from PIL import Image
 import pytesseract
-
-# Path to tesseract executable
 tesseract_path = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-# Path to the image or video
 image_path = r"C:\Users\User\Pictures\Screenshots\decision tree.png"
 
-# Open the image file
 img = Image.open(image_path)
 
-# Set the tesseract path in the pytesseract module
 pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
-# Use pytesseract to convert the image into text
 text = pytesseract.image_to_string(img)
 
-# Print the text
 print(text)
